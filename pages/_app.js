d2eaa2df10aa93f15434a76e2301e63511766d84
brokendished <brokendished@gmail.com>
2025-04-10 19:14:00 -0500

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,3 @@
-import '../styles/App.css';
-import '../styles/ChatbotChat.css';
-import '../styles/Header.css';
-
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
